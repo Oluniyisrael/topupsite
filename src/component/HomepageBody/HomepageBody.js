45323429d40ad15426ed47f251406684cbe4c40c
unknown <israeloluniyi2004@gmail.com>
2022-09-15 10:34:24 +0100

    chore: Visited and added some work commits

--- src/component/HomepageBody/HomepageBody.js
+++ src/component/HomepageBody/HomepageBody.js
@@ -5,12 +5,17 @@ export default class HomepageBody extends Component {
   render() {
     return (
       <div>
-        <div id=''>
-
+        <div id='item'>
+          
         </div>
         <div>
-          <p>Good day esteemed visitor! You being here means you clicked on my business link. I do top up your game for your various games that you can find them and their prices here. If you feel intrested in any of my products feel free to Contact me to order right away.
-          </p>
+          <div>
+            <p>Good day esteemed visitor! You being here means you clicked on my business link. I do top up your game for your various games that you can find them and their prices here. If you feel intrested in any of my products feel free to Contact me to order right away.
+            </p>
+          </div>
+          <div>
+
+          </div>
         </div>
       </div>
     )
